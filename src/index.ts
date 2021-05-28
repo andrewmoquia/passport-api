10050efea13cd11c83b72e90ead0b45b005a8073
andrewmoquia <80556699+andrewmoquia@users.noreply.github.com>
2021-05-28 21:03:28 +0800

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -168,7 +168,7 @@ app.get('/auth/google/callback',
 
 app.get('/auth/twitter', passport.authenticate('twitter'))
 
-qpp.get('/getUser/socmedway', (req, res) => {
+app.get('/getUser/socmedway', (req, res) => {
     res.send(req.user)
 })
 
