fcd91ba348c37e1261c506954e98923e5c2b6b33
andrewmoquia <80556699+andrewmoquia@users.noreply.github.com>
2021-05-29 17:47:04 +0800

    Add comment guide in app.ts

--- src/index.ts
+++ src/index.ts
@@ -16,14 +16,21 @@ import localRoutes from './passport/strategy/local/local.strategy'
 const app = express()
 
 //Middleware
+
+//Console verb action in HTTP
 app.use(morgan('dev'))
 app.use(express.json())
+
+//Site that allow to verb action in API
 app.use(cors({ origin: 'http://localhost:3000', credentials: true }))
 
+//ALlow test in localhost:4000
 app.set("trust proxy", 1)
 
+//Create cookie when interacting with API
 app.use(createSession)
 
+//Start passport middleware
 app.use(passport.initialize())
 app.use(passport.session())
 
@@ -33,10 +40,12 @@ passport.serializeUser(serialize)
 //Take user data and attaching it to req.user
 passport.deserializeUser(deserialize)
 
+//Hadle login routes verb action
 app.use(googleRoutes)
 app.use(twitterRoutes)
 app.use(localRoutes)
 
+//Authenticate request in secure routes
 app.use('/user', verify, secureRoute)
 
 app.listen(config.PORT || 4000, () => { console.log('Server is up!') })
