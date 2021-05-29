50063ac8e80346117a77e5f166c1042e95d7a4ac
andrewmoquia <80556699+andrewmoquia@users.noreply.github.com>
2021-05-29 13:37:31 +0800

    Update secure.routes.ts

--- src/secure.routes.ts
+++ src/secure.routes.ts
@@ -1,9 +1,15 @@
 const express = require('express');
 const router = express.Router();
 import {RequestHandler} from 'express'
+import User from './user'
 
-const getProfile: RequestHandler = (req, res, next) => {
-    res.send(req.user)
+const getProfile: RequestHandler = async (req, res, next) => {
+    try {
+        console.log(req.user)
+        const userFound = await User.findById({_id: req.user.SESSION})
+    } catch (error) {
+        console.log(error)
+    }
 }
 
 router.get('/profile', getProfile);
